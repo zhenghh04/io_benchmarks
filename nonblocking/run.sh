f456068e4a738bb807f9bd515448ad0f5cbead7c
Huihuo Zheng <zhenghh04@gmail.com>
2019-10-28 21:17:21 -0500

    local changes

--- nonblocking/run.sh
+++ nonblocking/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+#COBALT -A datascience -n 4 -t 1:00:00
 #Example to run the jobs
+#
 run=mpirun
-$run -np 4 prepare_datasets.x --num_batches 16 --batch_size 32
-$run -np 4 test_prefetch.x --num_batches 16 --batch_size 32 --compute_time 4
+#$run -np 48 -ppn 12 ./prepare_datasets.x --num_batches 480 --batch_size 512
+echo "==========================="
+$run -np 48 -ppn 12 ./test_prefetch.x --num_batches 480 --batch_size 512 --compute_time 0.5 --nonblocking 0 
+echo "==========================="
+$run -np 48 -ppn 12 ./test_prefetch.x --num_batches 480 --batch_size 512 --compute_time 0.5 --nonblocking 1 
