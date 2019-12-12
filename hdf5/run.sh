970ed1b06c919335ccc697fbea5fdb47b7e71f0e
Huihuo Zheng <zhenghh04@gmail.com>
2019-12-12 11:27:02 -0600

    added theta flags

--- hdf5/run.sh
+++ hdf5/run.sh
@@ -1,4 +1,6 @@
+#module load hdf5-ccio-intel-2019
+#lfs setstripe 
 export FOR_IGNORE_EXCEPTIONS=true
 export HDF5_CCIO_WR='yes'
 export HDF5_CCIO_CB_SIZE=1048576
-aprun -n 128 -j 2 ./parallel_hdf5.x --dim 128 $1
+mpirun -np 64  ./parallel_hdf5.x --dim 128 4096
