2aee055ffaa2d1d3798438ccaa84a0bbd3c0f206
Huihuo Zheng <huihuo.zheng@anl.gov>
2020-01-14 16:01:51 -0600

    local bebop

--- hdf5/run.sh
+++ hdf5/run.sh
@@ -1,6 +1,7 @@
 #module load hdf5-ccio-intel-2019
 #lfs setstripe 
 export FOR_IGNORE_EXCEPTIONS=true
-export HDF5_CCIO_WR='yes'
+export HDF5_CCIO_WR='no'
+export HDF5_CCIO_DEBUG='yes'
 export HDF5_CCIO_CB_SIZE=1048576
-mpirun -np 64  ./parallel_hdf5.x --dim 128 4096
+mpirun -np 2  ./parallel_hdf5.x --dim 4096 4096
