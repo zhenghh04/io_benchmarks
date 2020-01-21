69777b6e8468919dadda9c8986a6bed792835f9d
Huihuo Zheng <zhenghh04@gmail.com>
2020-01-21 14:51:30 +0000

    local changes

--- hdf5/run.sh
+++ hdf5/run.sh
@@ -1,4 +1,6 @@
 export FOR_IGNORE_EXCEPTIONS=true
 export HDF5_CCIO_WR='yes'
 export HDF5_CCIO_CB_SIZE=1048576
-aprun -n 128 -j 2 ./parallel_hdf5.x --dim 128 $1
+export HDF5_CCIO_DEBUG='yes'
+mpirun -n 4 ./parallel_hdf5.x --dim 4 10485760
+#aprun -n 128 -j 2 ./parallel_hdf5.x --dim 128 $1
