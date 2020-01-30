b6e5f02e2c830007c2b0b38fa12aeef7775abeb1
Huihuo Zheng <zhenghh04@gmail.com>
2020-01-30 08:21:54 -0600

    fixed bugs

--- hdf5/run.py
+++ hdf5/run.py
@@ -18,10 +18,9 @@ if hostname.find("theta")!=-1:
     root="/gpfs/mira-home/hzheng/io_benchmarks/hdf5/"
     parser.add_argument("--SSD", default="/local/scratch/", help="Directory for node local storage")
     parser.add_argument("--num_nodes", default=int(os.environ['COBALT_JOBSIZE']), type=int)
-    parser.add_argument("--ppn", default=16, type=int)
+    parser.add_argument("--ppn", default=32, type=int)
 else:
-    root="/home/hzheng/ExaHDF5/io_benchmarks/memory_maps/"
-#    root="/Users/zhenghh/Documents/Research/ExaHDF5/io_benchmarks/memory_maps/"
+    root="/Users/zhenghh/Documents/Research/ExaHDF5/io_benchmarks/hdf5/"
     parser.add_argument("--num_nodes", default=1, type=int)
     parser.add_argument("--SSD", default="SSD")
     parser.add_argument("--ppn", default=2, type=int)
@@ -34,7 +33,8 @@ parser.add_argument("--fsync",type=int, default=1)
 parser.add_argument("--directory", default="run/")
 parser.add_argument("--output", default="run.log")
 parser.add_argument("--collective", default=1, type=int)
-parser.add_argument("--cache", default=0, type=int);
+parser.add_argument("--cache", default=0, type=int)
+parser.add_argument("--sleep", default=0, type=float)
 args = parser.parse_args()
 exe=root+"parallel_hdf5.x"
 options = vars(args)
@@ -55,7 +55,7 @@ print(args.SSD)
 d1, d2 = [int(d) for d in args.dim.split()]
 if (args.cache==1) :
     exe = " -e SSD_CACHE=yes " + exe 
-extra_opts=" --filePerProc %s --fsync %s --collective %s" %(args.filePerProc, args.fsync, args.collective)
+extra_opts=" --filePerProc %s --fsync %s --collective %s --sleep %s" %(args.filePerProc, args.fsync, args.collective, args.sleep)
 if hostname.find("theta")!=-1:
     os.system("lfs setstripe -c %s -S %s %s"%(args.lustreStripeCount, args.lustreStripeSize, args.lustre))
     os.system("lfs getstripe %s"%args.lustre)
@@ -82,16 +82,11 @@ def read_to_str(fin, string):
         except:
             break
     return line
-exit()
 import json
 import datetime
 fin = open(root+args.directory+"/"+args.output, 'r')
 run = {}
 run['input'] = vars(args)
-run['SSD'] = [ [float(d.split()[0]), float(d.split()[2])] for d in read_to_str(fin, "SSD NODE").split(":")[1].split(',')[:-1]]
-run['mem->ssd'] = [float(d) for d in read_to_str(fin, "Write rate").split(":")[1].split()[0::2]]
-run['mem->lustre'] = [float(d) for d in read_to_str(fin, "Write rate").split(":")[1].split()[0::2]]
-run['mem->mmap'] =  [float(d) for d in read_to_str(fin, "Write rate").split(":")[1].split()[0::2]]
-run['mmap->ssd->lustre'] = [float(d) for d in read_to_str(fin, "Write rate").split(":")[1].split()[0::2]]
+run['write_rate'] = [float(d) for d in read_to_str(fin, "Write rate").split(":")[1].split()[0::2]]
 with open(root+args.directory+"/" + args.output.split('.')[0]+'.json', 'w') as f:
     json.dump(run, f)
