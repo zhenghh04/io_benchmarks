608820c200a01b8cc3a981248f22860680044967
Huihuo Zheng <zhenghh04@gmail.com>
2020-03-12 13:29:31 -0500

    modified test

--- read/test_mpi_win_attach.cpp
+++ read/test_mpi_win_attach.cpp
@@ -14,6 +14,7 @@
 #include <chrono>
 
 using namespace std;
+
 int main(int argc, char **argv) {
   MPI_Win win;
   size_t dim_a, dim_b, dim;
@@ -33,24 +34,21 @@ int main(int argc, char **argv) {
   }
   MPI_Win_create_dynamic(MPI_INFO_NULL, MPI_COMM_WORLD, &win);
   MPI_Win_attach(win, a, sizeof(int)*dim_a);
-  MPI_Win_attach(win, b, sizeof(int)*dim_b);
+  
   MPI_Win_fence(MPI_MODE_NOPUT, win);
   if (nproc > 1) {
     MPI_Get(&ta, 1, MPI_INT, (rank+1)%nproc, 0, 1, MPI_INT, win);
-    MPI_Get(&tb, 1, MPI_INT, (rank+1)%nproc, dim_a, 1, MPI_INT, win);
   } else {
     ta = a[0];
-    tb = b[0];
   }
   MPI_Win_fence(MPI_MODE_NOPUT, win);
   for(int i=0; i<nproc; i++)  {
     if (rank==i) {
-      cout << "rank " << rank << ":  ta - " << ta << endl;
-      cout << "rank " << rank << ":  tb - " << tb << endl;
+      cout << "rank " << rank << ":  ta - " << ta << " expected: " << (rank+1)%nproc << endl;
     }
     this_thread::sleep_for (chrono::seconds(1));
   }
-  MPI_Win_detach(win, a); free(a);
-  MPI_Win_detach(win, b); free(b);
+  MPI_Win_detach(win, a); delete [] a;
+  MPI_Win_free(&win); delete [] b; 
   MPI_Finalize();
 }
